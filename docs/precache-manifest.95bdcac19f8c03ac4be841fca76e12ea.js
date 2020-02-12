afe0e2caec835325e279e3deedcf9324dc09c168
docileone15 <60985777+docileone15@users.noreply.github.com>
2020-02-13 02:24:04 +0300

    3

---
+++ docs/precache-manifest.95bdcac19f8c03ac4be841fca76e12ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf86252887f304cf8fb1b12dac9665a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "966410620822d81f239d",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "ed5bcd4461723d8248c2",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "966410620822d81f239d",
+    "url": "./static/js/2.42281a19.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.42281a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5bcd4461723d8248c2",
+    "url": "./static/js/main.964a4ab5.chunk.js"
+  },
+  {
+    "revision": "0a7080d6e5055c82691d",
+    "url": "./static/js/runtime-main.8c7a8716.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
