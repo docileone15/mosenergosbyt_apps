1d25f6f372717171c2f7deb04fcfe4f2e41d2941
docileone15 <60985777+docileone15@users.noreply.github.com>
2020-02-13 21:07:43 +0300

    1

--- docs/index.js
+++ docs/index.js
@@ -5,12 +5,20 @@ import App from './App';
 // import registerServiceWorker from './sw';
 
 // Init VK  Mini App
+import connect from '@vkontakte/vk-connect';
+
+// Sends event to client
 connect.send('VKWebAppInit');
+
+// Subscribes to event, sended by client
+connect.subscribe(e => console.log(e));
 
 // Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
 // расскомментируйте строку с registerServiceWorker();
 // Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
 // Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
 // registerServiceWorker();
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- docs/panels/Home.js
+++ docs/panels/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import connect from '@vkontakte/vk-connect';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
@@ -7,6 +8,15 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+
+connect.subscribe((e) => console.log(e));
+
+<script src="dist/index.umd.js"></script>
+ 
+<script>
+  // Sends event to client
+  vkConnect.send('VKWebAppInit');
+</script> 
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
