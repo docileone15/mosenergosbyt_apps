2d102d0989dbe88fa1cc0aa15ccdd8b49c82a99b
docileone15 <60985777+docileone15@users.noreply.github.com>
2020-02-14 08:31:23 +0300

    1

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,18 +8,9 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
-const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Мосэнергосбыт — оплата электроэнергии</PanelHeader>
-		{fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+	
 
 		<br /><center><img src="http://pay.mosenergosbyt.ru/oplata/images/logo-new.png" width="320"/></center>
 		
